0d3407086b1d4d37ae8b72834271941b30739a28
axissixa <furiousdrake@gmail.com>
2019-04-08 19:22:36 +0530

    backendsubmit

--- application.js
+++ application.js
@@ -5,8 +5,13 @@ const errorHandler = require('./handlers/errorHandler');
 const logger = require('morgan');
 const app = express();
 
+app.engine('html', require('hbs').__express);
+app.set('view engine', 'html');
+//app.set('view engine', 'hbs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const v1 = require('./v1/index');
 
+const tryarr = [
+    {'a': "hello"},
+    {'a': "hello"},
+    {'a': "hello"},
+    {'a': "hello"},
+    {'a': "hello"},
+    {'a': "hello"},
+]
 router.get('/', (req,res)=> {
-    res.send({"status":"Application is working normally"});
+    res.render("layout"),{
+        categories: tryarr
+    }
+    //res.send({"status":"Application is working normally"});
 })
 
 // If the APIs are being updated to another version, its easier
